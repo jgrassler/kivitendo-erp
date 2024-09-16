c1980af65a994c97227501900be7cd4b307a0d5c
Johannes Grassler <info@computer-grassler.de>
2024-09-16 07:52:10 +0000

    Schnellupload fuer Dialog-/Kreditorenbuchungen
    Dieser Commit fuegt bei der Dialogbuchung und bei der
    Kreditorenbuchung eine Upload-Schaltflaeche fuer Dokumente
    hinzu. Diesr Schnellupload kann benutzt werden um gleich in
    der Hauptmaske einen Beleg mit hochzuladen ohne das nach
    erfolgter Buchung noch nachholen zu muessen.
    
    (cherry picked from commit d2837bc5cb8913f206815b294d1877256f2658b3)

--- js/kivi.AP.js
+++ js/kivi.AP.js
@@ -50,6 +50,13 @@ namespace('kivi.AP', function(ns){
     return true;
   };
 
+ns.update_file_name = function() {
+  let file = document.getElementById("file").files[0];
+  if (file) {
+    document.getElementById("file_name").value = file.name;
+  }
+};
+
 });
 
 $(function() {
