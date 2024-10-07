0050162d857ec73c3dcbc141e8e196a82744ee45
Johannes Grassler <info@computer-grassler.de>
2024-10-07 21:17:42 +0000

    Datenbankmigrationen: Abhaengigkeiten aufgeraeumt.

--- sql/Pg-upgrade2/defaults_margin_percentage.sql
+++ sql/Pg-upgrade2/defaults_margin_percentage.sql
@@ -1,5 +1,6 @@
 -- @tag: defaults_margin_percentage
 -- @description: New setting for default margin percentage. Will be preset in the Update Sell Price dialog.
+-- @depends: release_3_6_1
 
 ALTER TABLE defaults ADD COLUMN margin_percentage NUMERIC(8, 2);
 UPDATE defaults SET margin_percentage = 1.0;
